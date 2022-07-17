342ce8dc593a8373445f63a40729d211b0dd769d
shj2022 <106890894+shj2022@users.noreply.github.com>
2022-07-16 21:35:21 -0400

    Update main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-//  copy & paste the below from ML training code
+//  copy & paste the below from the ML training code
 let scaler_mean = [-177.48559249786868, 927.1557118499572, -163.73593350383635, 313.40030982357086, 352.0293421898865, 359.9212856332229, -804.4117647058823, 282.70588235294116, -756.6419437340154, 435.17306052855923, 1532.8729752770673, 548.995737425405]
 let scaler_sd = [122.81336316110112, 155.35774130311398, 172.79928738733565, 325.4941488616266, 338.8675980850038, 436.1055304406833, 661.4673896097793, 760.6671341279181, 782.6163916969433, 722.2878198389716, 422.2618853350948, 743.0749119638036]
 let coef = [[0.09072921892541963, 0.02050763109394002, 0.07990760096870853, 0.1959510865053412, 0.24749407547432828, 0.08125397351231137, -0.15311007915439134, -0.18676970998309272, -0.10963787716854928, 0.26096017201491933, 0.6495797112134594, 0.14765281352724935], [0.9647705673104152, -0.9381255709889622, 1.0963808461538243, -0.5105358086653857, -2.8208343897965533, 2.733823393949729, -0.9638646413982466, -0.3980405455498639, -2.039661913627575, 4.249620325028033, 2.568703600692576, 2.0216968594194626], [-2.119320083729022, -4.14245613826354, 1.7671070112006362, -0.6863723259367873, -3.817945787910247, -0.42490535934352636, -0.8246324422987357, 0.7537638537456814, -0.44004469929569723, -0.16889468247695305, -5.910635423913789, -0.4925098534128196]]
@@ -8,6 +8,8 @@ let activities = ["r", "s", "w"]
 let rawX : number[] = []
 let rawY : number[] = []
 let rawZ : number[] = []
+let current_activity = "start"
+//  define mathematical functions
 function average(list1: number[]): number {
     let sum = 0
     for (let value of list1) {
@@ -40,7 +42,7 @@ function maximum(list4: number[]): number {
     return val2
 }
 
-// feature_package order: avg x,y,z, sd x,y,z, min x,y,z, max x,y,z
+// define features: feature_package order: avg x,y,z, sd x,y,z, min x,y,z, max x,y,z
 function feature_package(listx: number[], listy: number[], listz: number[]): any[] {
     let to_return = []
     to_return.push((average(listx) - scaler_mean[0]) / scaler_sd[0])
@@ -58,7 +60,7 @@ function feature_package(listx: number[], listy: number[], listz: number[]): any
     return to_return
 }
 
-//  function that predicts class of newdata(array with 12 elements)
+//  define function that predicts class of a feature package (array of 12 elements computed from 20 data points 0.1 sec apart)
 function predict(list5: number[]): number {
     let i: number;
     let dot_product: number;
@@ -96,7 +98,9 @@ function predict(list5: number[]): number {
     return _py.py_array_index(count, maximum(count))
 }
 
-let current_activity : string[] = []
+//  every 0.1 sec: collect acceleration data, update     
+// serial.write_line(current_activity[len(current_activity)-1])
+// current_activity: List[string] = []
 for (let index = 0; index < 19; index++) {
     rawX.push(input.acceleration(Dimension.X))
     rawY.push(input.acceleration(Dimension.Y))
@@ -108,9 +112,12 @@ basic.forever(function on_forever() {
     rawY.push(input.acceleration(Dimension.Y))
     rawZ.push(input.acceleration(Dimension.Z))
     basic.pause(97)
-    current_activity.push(activities[predict(feature_package(rawX, rawY, rawZ))])
-    basic.showString(current_activity[current_activity.length - 1], 1)
+    let current_activity = activities[predict(feature_package(rawX, rawY, rawZ))]
+    // current_activity.append(activities[predict(feature_package(rawX, rawY, rawZ))])    
+    // basic.show_string(current_activity[len(current_activity)-1],1)
+    basic.showString(current_activity, 1)
     rawX.removeAt(0)
     rawY.removeAt(0)
     rawZ.removeAt(0)
+    serial.writeLine(current_activity)
 })
